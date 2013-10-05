1b9dc584d5c12cb79f534e8091ffbe8a361a44ee
Comrod <icomrod@gmail.com>
2013-10-05 14:47:16 -0500

    Fucking around

---
+++ C++FuckingAround/C++FuckingAround/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  C++FuckingAround
+//
+//  Created by Cormac Chester on 10/5/13.
+//  Copyright (c) 2013 Testman Industries. All rights reserved.
+//
+
+#include <iostream>
+#define tax_rate 0.06
+#define shipping 0.10
+
+int main()
+{
+    
+}
